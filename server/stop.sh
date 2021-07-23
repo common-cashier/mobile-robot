e17e39fa6d61b5ff63adb46e8cf670e5f31b100b
payne dong <leipayne18@gmail.com>
2021-07-23 14:42:26 +0800

    stop sh

---
+++ server/stop.sh
@@ -0,0 +1,17 @@
+GATEWAY=`ps -ef|grep gateway.py|grep -v grep|awk '{print $2}'`
+for pid in $GATEWAY
+do
+  kill -9 $pid
+done
+
+#WEBSOCK=`ps -ef|grep websock.py|grep -v grep|awk '{print $2}'`
+#for pid in $WEBSOCK
+#do
+#  kill -9 $pid
+#done
+#
+#ROOT_SERVICE=`ps -ef|grep root_service.py|grep -v grep|awk '{print $2}'`
+#for pid in $ROOT_SERVICE
+#do
+#  kill -9 $pid
+#done
