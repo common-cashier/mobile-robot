ceec23920b2199b3d636a99443163d10a764bd67
common-cashier <common.free.cashier@gmail.com>
2021-08-27 10:56:00 +0800

    stop 2

--- server/bot_factory.py
+++ server/bot_factory.py
@@ -72,8 +72,8 @@ class BotFactory:
             robot = getattr(module, settings.bot.bank.lower())
             self.bank = robot
         if params['do_work'] == "stop":
-            self.d.app_stop_all()
             api.status(params['account_alias'], settings.Status.IDLE)
+            self.d.app_stop_all()
             # return self.bank.get_package()
         if params['do_work'] == "go_to_transfer":
             settings.order_exists = cast_query_order(settings.bot.account.alias)
