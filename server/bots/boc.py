b656048c4140e9e0c7a2102063110db44ff2bc5e
common-cashier <common.free.cashier@gmail.com>
2021-09-07 12:24:20 +0800

    dd delay before submit

--- server/bots/boc.py
+++ server/bots/boc.py
@@ -171,12 +171,17 @@ def transfer():
                 input_amount()
                 self(resourceId="com.chinamworld.bocmbci:id/trans_remit_payeename").click()
                 self.send_keys(settings.transferee.holder, clear=True)
+                self.sleep(2)
                 self(resourceId="com.chinamworld.bocmbci:id/clear_edit_context").click()
                 self.send_keys(settings.transferee.account, clear=True)
+                self.sleep(2)
                 if self(resourceId="com.chinamworld.bocmbci:id/choice_data_arrow", description="点击更换收款银行").exists(
                         timeout=5):
+                    self.sleep(2)
                     self(resourceId="com.chinamworld.bocmbci:id/choice_data_arrow", description="点击更换收款银行").click()
+                    self.sleep(2)
                     self.swipe_ext("up", scale=0.8)
+                    self.sleep(2)
                 # if self.xpath('//*[@resource-id="com.chinamworld.bocmbci:id/trans_remit_openbank"]/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]/android.widget.ImageView[1]').exists:
                 #     self.xpath('//*[@resource-id="com.chinamworld.bocmbci:id/trans_remit_openbank"]/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]/android.widget.ImageView[1]').click()
                 #     self.swipe_ext("up", scale=0.8)
